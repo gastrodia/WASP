011b083a25d4da75470365af92ddbdaafc9ab5ff
许阳 <gastrodia@outlook.com>
2015-10-28 22:24:38 +0800

    Object3d.ts

---
+++ Engine/Source/Runtime/Core/Channels.ts
@@ -0,0 +1,27 @@
+class Channels{
+  mask = 1;
+  set( channel ) {
+
+		this.mask = 1 << channel;
+
+	}
+
+	enable( channel ) {
+
+		this.mask |= 1 << channel;
+
+	}
+
+	toggle( channel ) {
+
+		this.mask ^= 1 << channel;
+
+	}
+
+	disable ( channel ) {
+
+		this.mask &= ~ ( 1 << channel );
+
+	}
+
+}
