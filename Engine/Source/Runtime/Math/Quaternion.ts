b0bd24f051d66517e26e529995e7c3ef319bc362
许阳 <gastrodia@outlook.com>
2015-11-09 13:24:17 +0800

    Quaternion.ts

--- Engine/Source/Runtime/Math/Quaternion.ts
+++ Engine/Source/Runtime/Math/Quaternion.ts
@@ -1,5 +1,200 @@
+import Euler = require('./Euler');
+import Matrix3 = require('./Matrix3');
 class Quaternion{
+  private _x:number;
+  private _y:number;
+  private _z:number;
+  private _w:number;
+  constructor(x:number,y:number,z:number,w?:number){
+    this._x = x || 0;
+    this._y = y || 0;
+    this._z = z || 0;
+    this._w = (w!==undefined) ? w :1;
+  }
 
+  get x(){
+    return this._x;
+  }
+
+  set x(value:number){
+    this._x = value;
+    this.onChangeCallback();
+  }
+
+  get y(){
+    return this._y;
+  }
+
+  set y(value:number){
+    this._y = value;
+    this.onChangeCallback();
+  }
+
+  get z(){
+    return this._z;
+  }
+
+  set z(value:number){
+    this._z = value;
+    this.onChangeCallback();
+  }
+
+  get w(){
+    return this._w;
+  }
+
+  set w(value:number){
+    this._w = value;
+    this.onChangeCallback();
+  }
+
+  set(x:number,y:number,z:number,w:number){
+    this._x = x;
+    this._y = y;
+    this._z = z;
+    this._w = w;
+    this.onChangeCallback();
+    return this;
+  }
+
+  clone(){
+    return new Quaternion(this._x,this._y,this._z,this._w);
+  }
+
+  copy(quaternion:Quaternion){
+    this._x = quaternion.x;
+    this._y = quaternion.y;
+    this._z = quaternion.z;
+    this._w = quaternion.w;
+    this.onChangeCallback();
+    return this;
+  }
+
+  setFromEuler(euler:Euler,update:boolean){
+    var c1 = Math.cos(euler.x / 2);
+    var c2 = Math.cos(euler.y / 2);
+    var c3 = Math.cos(euler.z / 2);
+    var s1 = Math.sin(euler.x / 2);
+    var s2 = Math.sin(euler.y / 2);
+    var s3 = Math.sin(euler.z / 2);
+
+    var order = euler.order;
+
+    if ( order === 'XYZ' ) {
+
+			this._x = s1 * c2 * c3 + c1 * s2 * s3;
+			this._y = c1 * s2 * c3 - s1 * c2 * s3;
+			this._z = c1 * c2 * s3 + s1 * s2 * c3;
+			this._w = c1 * c2 * c3 - s1 * s2 * s3;
+
+		} else if ( order === 'YXZ' ) {
+
+			this._x = s1 * c2 * c3 + c1 * s2 * s3;
+			this._y = c1 * s2 * c3 - s1 * c2 * s3;
+			this._z = c1 * c2 * s3 - s1 * s2 * c3;
+			this._w = c1 * c2 * c3 + s1 * s2 * s3;
+
+		} else if ( order === 'ZXY' ) {
+
+			this._x = s1 * c2 * c3 - c1 * s2 * s3;
+			this._y = c1 * s2 * c3 + s1 * c2 * s3;
+			this._z = c1 * c2 * s3 + s1 * s2 * c3;
+			this._w = c1 * c2 * c3 - s1 * s2 * s3;
+
+		} else if ( order === 'ZYX' ) {
+
+			this._x = s1 * c2 * c3 - c1 * s2 * s3;
+			this._y = c1 * s2 * c3 + s1 * c2 * s3;
+			this._z = c1 * c2 * s3 - s1 * s2 * c3;
+			this._w = c1 * c2 * c3 + s1 * s2 * s3;
+
+		} else if ( order === 'YZX' ) {
+
+			this._x = s1 * c2 * c3 + c1 * s2 * s3;
+			this._y = c1 * s2 * c3 + s1 * c2 * s3;
+			this._z = c1 * c2 * s3 - s1 * s2 * c3;
+			this._w = c1 * c2 * c3 - s1 * s2 * s3;
+
+		} else if ( order === 'XZY' ) {
+
+			this._x = s1 * c2 * c3 - c1 * s2 * s3;
+			this._y = c1 * s2 * c3 - s1 * c2 * s3;
+			this._z = c1 * c2 * s3 + s1 * s2 * c3;
+			this._w = c1 * c2 * c3 + s1 * s2 * s3;
+
+		}
+
+    if ( update !== false ) this.onChangeCallback();
+    return this;
+  }
+
+  setFromAxisAngle(axis,angle){
+    var halfAngle = angle / 2, s= Math.sin(halfAngle);
+    this._x = axis.x * s;
+    this._y = axis.y * s;
+    this._z = axis.z * s;
+    this._w = Math.cos(halfAngle);
+
+    this.onChangeCallback();
+    return this;
+  }
+
+  setFromRotationMatrix(m:Matrix3){
+    var te = m.elements,
+
+			m11 = te[ 0 ], m12 = te[ 4 ], m13 = te[ 8 ],
+			m21 = te[ 1 ], m22 = te[ 5 ], m23 = te[ 9 ],
+			m31 = te[ 2 ], m32 = te[ 6 ], m33 = te[ 10 ],
+
+			trace = m11 + m22 + m33,
+			s;
+
+		if ( trace > 0 ) {
+
+			s = 0.5 / Math.sqrt( trace + 1.0 );
+
+			this._w = 0.25 / s;
+			this._x = ( m32 - m23 ) * s;
+			this._y = ( m13 - m31 ) * s;
+			this._z = ( m21 - m12 ) * s;
+
+		} else if ( m11 > m22 && m11 > m33 ) {
+
+			s = 2.0 * Math.sqrt( 1.0 + m11 - m22 - m33 );
+
+			this._w = ( m32 - m23 ) / s;
+			this._x = 0.25 * s;
+			this._y = ( m12 + m21 ) / s;
+			this._z = ( m13 + m31 ) / s;
+
+		} else if ( m22 > m33 ) {
+
+			s = 2.0 * Math.sqrt( 1.0 + m22 - m11 - m33 );
+
+			this._w = ( m13 - m31 ) / s;
+			this._x = ( m12 + m21 ) / s;
+			this._y = 0.25 * s;
+			this._z = ( m23 + m32 ) / s;
+
+		} else {
+
+			s = 2.0 * Math.sqrt( 1.0 + m33 - m11 - m22 );
+
+			this._w = ( m21 - m12 ) / s;
+			this._x = ( m13 + m31 ) / s;
+			this._y = ( m23 + m32 ) / s;
+			this._z = 0.25 * s;
+
+		}
+
+		this.onChangeCallback();
+
+		return this;
+  }
+
+  onChangeCallback(){
+
+  }
 }
 
 export = Quaternion;
