204dc2ac4fbe5c1b97abba66dc4f2c67a8b4788a
许阳 <gastrodia@outlook.com>
2015-11-28 21:30:23 +0800

    回收有毒脚本

---
+++ script/git/submit_submodules.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+workpath=$(cd `dirname $0`; pwd)/
+cd ${workpath}
+
+cd ../
+cd Engine
+git config receive.denyCurrentBranch updateInstead
+git checkout master
+git pull
+git add -A
+git commit -m 'submit submodule'
+git push
+
+cd ../
+cd tsv2_res
+git config receive.denyCurrentBranch updateInstead
+git checkout master
+git pull
+git add -A
+git commit -m 'submit submodule'
+git push
